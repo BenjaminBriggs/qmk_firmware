dcd2bee3f3ee09c654590d01ec93646dfae323cf
Benjamin Briggs <ben@palringo.com>
2017-01-20 16:32:01 +0000

    Add more xcode controls

--- keyboards/ergodox/keymaps/swift-osx/keymap.c
+++ keyboards/ergodox/keymaps/swift-osx/keymap.c
@@ -2,6 +2,9 @@
 #include "debug.h"
 #include "action_layer.h"
 
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
+
 #define BASE 0
 #define NAVI 1
 #define SYMB 2
@@ -18,30 +21,33 @@
 #define XC_DOWN LALT(LGUI(KC_RBRC))
 #define XC_LEFT LGUI(KC_LBRC)
 #define XC_RIGHT LGUI(KC_RBRC)
+#define XC_EDIT LCTL(LGUI(KC_E))
 
 #define BIG_UP M(0)
 #define BIG_DOWN M(1)
 
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = KEYMAP(
-    KC_ESC,  KC_1,     KC_2,  KC_3,    KC_4,    KC_5,    KC_MINS,
-    KC_TAB,  KC_Q,     KC_W,  KC_E,    KC_R,    KC_T,    LGUI(KC_LBRC),
-    KC_LCTL, KC_A,     KC_S,  KC_D,    KC_F,    KC_G,
-    KC_LSFT, KC_Z,     KC_X,  KC_C,    KC_V,    KC_B,    LGUI(LCTL(KC_SPC)),
-    KC_GRAVE,KC_QUOTE, KC_HASH,KC_LALT,   KC_LGUI,
-    /*-*/    /*-*/     /*-*/  /*-*/    /*-*/    KC_MUTE, KC_VOLD,
-    /*-*/    /*-*/     /*-*/  /*-*/    /*-*/    /*-*/    KC_VOLU,
-    /*-*/    /*-*/     /*-*/  /*-*/    KC_BSPC, GUI_T(KC_SPC), MO(SYMB),
+    KC_ESC,  KC_1,     KC_2,   KC_3,    KC_4,    KC_5,    KC_MINS,
+    KC_TAB,  KC_Q,     KC_W,   KC_E,    KC_R,    KC_T,    HYPR(KC_F20),
+    KC_LCTL, KC_A,     KC_S,   KC_D,    KC_F,    KC_G,
+    KC_LSFT, KC_Z,     KC_X,   KC_C,    KC_V,    KC_B,    LGUI(LCTL(KC_SPC)),
+    KC_GRAVE,KC_QUOTE, KC_HASH,KC_LALT, KC_LGUI,
+    /*-*/    /*-*/     /*-*/   /*-*/    /*-*/    KC_MUTE, KC_VOLD,
+    /*-*/    /*-*/     /*-*/   /*-*/    /*-*/    /*-*/    KC_VOLU,
+    /*-*/    /*-*/     /*-*/   /*-*/    KC_BSPC, KC_SPC,  MO(SYMB),
     //
     //
     /*-*/    KC_PLUS,  KC_6,  KC_7,    KC_8,    KC_9,    KC_0,       KC_ESC,
-    /*-*/ LGUI(KC_RBRC),KC_Y, KC_U,    KC_I,    KC_O,    KC_P,       KC_MINS,
+    /*-*/ HYPR(KC_F21),KC_Y, KC_U,    KC_I,    KC_O,    KC_P,       KC_MINS,
     /*-*/    /*-*/     KC_H,  KC_J,    KC_K,    KC_L,    KC_SCLN,    KC_RCTL,
     /*-*/ LGUI(KC_SPC),KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    KC_RSFT,
-    /*-*/    /*-*/     /*-*/  KC_RGUI, KC_RALT, LY_SYMB, KC_HYPR,    MO(SWIFT),
+    /*-*/    /*-*/     /*-*/  KC_RGUI, KC_RALT, KC_LBRC, KC_RBRC,    MO(SWIFT),
     KC_MPRV, KC_MNXT,
     KC_MPLY,
-    MO(NAVI), GUI_T(KC_SPC),  KC_ENT
+    MO(NAVI), KC_RSFT,  KC_ENT
   ),
   [SYMB] = KEYMAP(
     KC_TRNS, KC_EXLM,  KC_AT  , LSFT(KC_3),  KC_DLR , KC_PERC, KC_UNDS,
@@ -64,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     MO(SWIFT), KC_TRNS, KC_TRNS
   ),
   [NAVI] = KEYMAP(
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -74,8 +80,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*-*/    /*-*/    /*-*/    /*-*/    KC_TRNS, KC_TRNS, MO(SWIFT),
     //
     //
-    /*-*/    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    /*-*/    KC_TRNS, KC_TRNS, BIG_UP,  KC_UP,   BIG_DOWN,KC_TRNS, KC_TRNS,
+    /*-*/    KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+    /*-*/    KC_TRNS, KC_TRNS, BIG_UP,  KC_UP,   BIG_DOWN,KC_TRNS, KC_F12,
     /*-*/    /*-*/    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,
     /*-*/    KC_TRNS, KC_N,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     /*-*/    /*-*/    /*-*/    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -85,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [SWIFT] = KEYMAP(
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, XC_EDIT, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
